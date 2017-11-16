04b9a2dd4c4bc64257e8aacd621e79a02055ca7f
Luni-4 <luni-4@hotmail.it>
2017-11-16 19:26:04 +0100

    Update main

--- src/main.cu
+++ src/main.cu
@@ -4,9 +4,7 @@
 #include "Windows.h"
 #endif
 
-//#ifdef DEBUG
 #include "Common.h"
-//#endif
 
 // Librerie di progetto
 #include "Mnist.h"
@@ -15,70 +13,21 @@
 #include "Network.h"
 
 #ifdef DEBUG
-
-void test_input() {
-	// Leggere i dati
-	Data* d = new Mnist("data/");
-
-	// Lettura dati di training
-	d->readTrainData();
-
-	delete d;
-}
-
-
-void test_fully() {
-	LayerDefinition* layer = new FullyConnected(10, RELU);
-
-	// Stampa dei vari parametri del livello
-	printf("Larghezza layer: %d\n", layer->getWidth());
-	printf("Altezza layer: %d\n", layer->getHeight());
-	printf("Tipo di layer: %d\n", layer->getLayerType());
-	printf("Funzione di attivazione: %d", layer->getActivationFunction());
-
-	// Definizione di un'immagine di 4 pixel rgb
-	layer->defineCuda(2, 2, 3);
-
-	double *inp;
-
-	CHECK(cudaMalloc((void**)&inp, 12 * sizeof(double)));
-
-	CHECK(cudaMemset(inp, 0, 12 * sizeof(double)));
-
-	std::cout << "\n\n\nImmagine di input RGB\n\n";
-	printFromCuda(inp, 12);
-
-	// Passare l'immagine ed eseguire prodotto più aggiunta del bias
-	layer->forward_propagation(inp);
-
-	std::vector<double> w = layer->getWeights();
-
-	std::cout << "\n\n\nNumero dei pesi: ";
-	std::cout << w.size();
-	std::cout << "\n\nPesi\n\n";
-
-	for (auto t : w)
-		std::cout << t << std::endl;
-
-	layer->deleteCuda();
-
-	CHECK(cudaFree(inp));
-
-	delete layer;
-}
-
+#include "Test.h"
 #endif
 
 int main() {
 
 #ifdef DEBUG
-	//test_input();
+	test_mnist_input();
+	
+	test_cifar_input();
 
-	//test_fully();
+	test_fully();
 #endif
 
 	// Leggere i dati
-	std::unique_ptr<Data> d(new Mnist("data/"));
+	std::unique_ptr<Data> d(new Mnist("data/mnist/"));
 
 	// Vettore contenente i livelli della rete
 	std::vector<std::unique_ptr<LayerDefinition>> layers;
@@ -114,7 +63,7 @@ int main() {
 	printLabels(predictions);
 	
 	// Stampare l'errore
-	std::cout << "\n\n" << error << std::endl;	
+	std::cout << std::endl << std::endl << error << std::endl;	
 
 #ifdef _WIN32
 	system("pause");
