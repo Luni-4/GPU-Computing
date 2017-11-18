5b8a9c3c3b8ee5e70143873530f07944c3cec17f
Luni-4 <luni-4@hotmail.it>
2017-11-18 14:30:31 +0100

    Fix inconsistencies

--- src/Kernel.h
+++ src/Kernel.h
@@ -8,18 +8,18 @@
 #endif
 
 namespace Kernel {
-	void initWeightK(dim3 t, dim3 b, double *weight, const int &wDim, curandState *states);
+	void initWeightK(dim3 b, dim3 t, double *weight, const int &wDim, curandState *states);
 
-	void initBiasK(dim3 t, dim3 b, double *weight, const int &wDim, curandState *states);
+	void initBiasK(dim3 b, dim3 t, double *weight, const int &wDim, curandState *states);
 
-	void outputErrorK(dim3 t, dim3 b, const double *output, double *error, const uint8_t *label, const int &target, const int &nodes);
+	void outputErrorK(dim3 b, dim3 t, const double *output, double *error, const uint8_t *label, const int &target, const int &nodes);
 
-	void actReluK(dim3 t, dim3 b, double *output, const int &nodes);
-	void derivActReluK(dim3 t, dim3 b, const double *output, double *error, const int &nodes);
+	void actReluK(dim3 b, dim3 t, double *output, const int &nodes);
+	void derivActReluK(dim3 b, dim3 t, const double *output, double *error, const int &nodes);
 
-	void actSigmoidK(dim3 t, dim3 b, double *output, const int &nodes);
-	void derivActSigmoidK(dim3 t, dim3 b, const double *output, double *error, const int &nodes);
+	void actSigmoidK(dim3 b, dim3 t, double *output, const int &nodes);
+	void derivActSigmoidK(dim3 b, dim3 t, const double *output, double *error, const int &nodes);
 
-	void actTanhK(dim3 t, dim3 b, double *output, const int &nodes);
-	void derivActTanhK(dim3 t, dim3 b, const double *output, double *error, const int &nodes);
+	void actTanhK(dim3 b, dim3 t, double *output, const int &nodes);
+	void derivActTanhK(dim3 b, dim3 t, const double *output, double *error, const int &nodes);
 }
