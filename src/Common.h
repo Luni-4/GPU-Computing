8ea7438b251207a9725dafd529c1f3dc777a7897
Luni-4 <luni-4@hotmail.it>
2017-11-27 23:27:01 +0100

    Update Headers

--- src/Common.h
+++ src/Common.h
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include <fstream>
 #include <ctime>
 #include <chrono>
--- src/Mnist.cu
+++ src/Mnist.cu
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <iterator>
 #include <algorithm>
 
 #include "Mnist.h"
