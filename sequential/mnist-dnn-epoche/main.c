e7ade591322e692c673153e4152afa10ed570b98
Lemen47Nic <aspes.nicholas@hotmail.it>
2018-01-07 15:01:27 +0100

    change dnn network

--- sequential/mnist-dnn-epoche/main.c
+++ sequential/mnist-dnn-epoche/main.c
@@ -19,9 +19,7 @@
  */
 
 
-
-
-// Include external libraries
+ // Include external libraries
 #include <stdlib.h>
 #include <time.h>
 #include <stdarg.h>
@@ -43,50 +41,50 @@
  * @param nn A pointer to the network
  */
 
-void trainNetwork(Network *nn){
-    
-    // open MNIST files
-    FILE *imageFile, *labelFile;
-    imageFile = openMNISTImageFile(MNIST_TRAINING_SET_IMAGE_FILE_NAME);
-    labelFile = openMNISTLabelFile(MNIST_TRAINING_SET_LABEL_FILE_NAME);
-    
-    int errCount = 0;
-    
-    int epoch = 20;
-    
-    // Loop through all images in the file
-    for (int imgCount=0; imgCount<MNIST_MAX_TRAINING_IMAGES; imgCount++){
-        
-        // Reading next image and its corresponding label
-        MNIST_Image img = getImage(imageFile);
-        MNIST_Label lbl = getLabel(labelFile);
-        
-        // Convert the MNIST image to a standardized vector format and feed into the network
-        Vector *inpVector = getVectorFromImage(&img);
-        feedInput(nn, inpVector);
-        
-        for(int i = 0; i < epoch; i++){
+void trainNetwork(Network *nn) {
 
-            // Feed forward all layers (from input to hidden to output) calculating all nodes' output
-            feedForwardNetwork(nn);
+	// open MNIST files
+	FILE *imageFile, *labelFile;
+	imageFile = openMNISTImageFile(MNIST_TRAINING_SET_IMAGE_FILE_NAME);
+	labelFile = openMNISTLabelFile(MNIST_TRAINING_SET_LABEL_FILE_NAME);
 
-            // Back propagate the error and adjust weights in all layers accordingly
-            backPropagateNetwork(nn, lbl);
-       }
+	int errCount = 0;
 
-        // Classify image by choosing output cell with highest output
-        int classification = getNetworkClassification(nn);
-        if (classification!=lbl) errCount++;
+	int epoch = 1;
 
-        // Display progress during training
-        displayTrainingProgress(imgCount, errCount);
+	// Loop through all images in the file
+	for (int imgCount = 0; imgCount < MNIST_MAX_TRAINING_IMAGES; imgCount++) {
 
-    }
-    
-    // Close files
-    fclose(imageFile);
-    fclose(labelFile);
-    
+		// Reading next image and its corresponding label
+		MNIST_Image img = getImage(imageFile);
+		MNIST_Label lbl = getLabel(labelFile);
+
+		// Convert the MNIST image to a standardized vector format and feed into the network
+		Vector *inpVector = getVectorFromImage(&img);
+		feedInput(nn, inpVector);
+
+		for (int i = 0; i < epoch; i++) {
+
+			// Feed forward all layers (from input to hidden to output) calculating all nodes' output
+			feedForwardNetwork(nn);
+
+			// Back propagate the error and adjust weights in all layers accordingly
+			backPropagateNetwork(nn, lbl);
+		}
+
+		// Classify image by choosing output cell with highest output
+		int classification = getNetworkClassification(nn);
+		if (classification != lbl) errCount++;
+
+		// Display progress during training
+		displayTrainingProgress(imgCount, errCount);
+
+	}
+
+	// Close files
+	fclose(imageFile);
+	fclose(labelFile);
+
 }
 
 
@@ -98,41 +96,41 @@ void trainNetwork(Network *nn){
  * @param nn A pointer to the network
  */
 
-void testNetwork(Network *nn){
-    
-    // open MNIST files
-    FILE *imageFile, *labelFile;
-    imageFile = openMNISTImageFile(MNIST_TESTING_SET_IMAGE_FILE_NAME);
-    labelFile = openMNISTLabelFile(MNIST_TESTING_SET_LABEL_FILE_NAME);
-    
-    int errCount = 0;
-    
-    // Loop through all images in the file
-    for (int imgCount=0; imgCount<MNIST_MAX_TESTING_IMAGES; imgCount++){
-        
-        // Reading next image and its corresponding label
-        MNIST_Image img = getImage(imageFile);
-        MNIST_Label lbl = getLabel(labelFile);
-        
-        // Convert the MNIST image to a standardized vector format and feed into the network
-        Vector *inpVector = getVectorFromImage(&img);
-        feedInput(nn, inpVector);
-        
-        // Feed forward all layers (from input to hidden to output) calculating all nodes' output
-        feedForwardNetwork(nn);
-        
-        // Classify image by choosing output cell with highest output
-        int classification = getNetworkClassification(nn);
-        if (classification!=lbl) errCount++;
-        
-        // Display progress during testing
-        displayTestingProgress(imgCount, errCount);
-    }
-    
-    // Close files
-    fclose(imageFile);
-    fclose(labelFile);
-    
+void testNetwork(Network *nn) {
+
+	// open MNIST files
+	FILE *imageFile, *labelFile;
+	imageFile = openMNISTImageFile(MNIST_TESTING_SET_IMAGE_FILE_NAME);
+	labelFile = openMNISTLabelFile(MNIST_TESTING_SET_LABEL_FILE_NAME);
+
+	int errCount = 0;
+
+	// Loop through all images in the file
+	for (int imgCount = 0; imgCount < MNIST_MAX_TESTING_IMAGES; imgCount++) {
+
+		// Reading next image and its corresponding label
+		MNIST_Image img = getImage(imageFile);
+		MNIST_Label lbl = getLabel(labelFile);
+
+		// Convert the MNIST image to a standardized vector format and feed into the network
+		Vector *inpVector = getVectorFromImage(&img);
+		feedInput(nn, inpVector);
+
+		// Feed forward all layers (from input to hidden to output) calculating all nodes' output
+		feedForwardNetwork(nn);
+
+		// Classify image by choosing output cell with highest output
+		int classification = getNetworkClassification(nn);
+		if (classification != lbl) errCount++;
+
+		// Display progress during testing
+		displayTestingProgress(imgCount, errCount);
+	}
+
+	// Close files
+	fclose(imageFile);
+	fclose(labelFile);
+
 }
 
 
@@ -143,78 +141,79 @@ void testNetwork(Network *nn){
  */
 
 int main(int argc, const char * argv[]) {
-    
-    // remember the time in order to calculate processing time at the end
-    time_t startTime = time(NULL);
-    
-    // set locale (for outputting formatted numbers/dates/strings)
-    setlocale(LC_ALL, "En_US");
-    
-    // clear screen of terminal window and display title
-    clearScreen();
-    printf("MNIST-DNN: A deep neural network for MNIST image recognition \n\n");
 
-    // Define the network's overall architecture (layers, nodes, activation function, etc.)
-    
-    // Define how many layers
-    int numberOfLayers = 4;
+	// remember the time in order to calculate processing time at the end
+	time_t startTime = time(NULL);
 
-    // Define the network model as individual layers (layer by layer)
-    LayerDefinition inputLayer = {
-        .layerType       = INPUT,
-        .nodeMap         = (Volume){.width=MNIST_IMG_WIDTH, .height=MNIST_IMG_HEIGHT}
-    };
-    
-    LayerDefinition hiddenLayer = {
-        .layerType       = CONVOLUTIONAL,
-        .activationType  = RELU,
-        .nodeMap         = (Volume){.width=13, .height=13, .depth=5},
-        .filter          = 5
-    };
-    
-    LayerDefinition hiddenLayer2 = {
-        .layerType       = CONVOLUTIONAL,
-        .activationType  = RELU,
-        .nodeMap         = (Volume){.width=6, .height=6, .depth=5},
-        .filter          = 3
-    };
-    
-    LayerDefinition outputLayer = {
-        .layerType       = OUTPUT,
-        .activationType  = RELU,
-        .nodeMap         = (Volume){.width=10}
-    };
-    
-    // Create an array to hold all of the above layer definitions (for easier reference throught the code)
-    LayerDefinition *layerDefs = setLayerDefinitions(numberOfLayers, inputLayer, hiddenLayer, hiddenLayer2, outputLayer);
-    
-    // Display details of the network definition/architecture on the screen
-    outputNetworkDefinition(numberOfLayers, layerDefs);
-    
-    // Create a neural network based on the above definition
-    Network *nn = createNetwork(numberOfLayers, layerDefs);
-    
-    // Define additional hyper-parameters (optional)
-    nn->learningRate = 0.0050;
-    
-    // Train the network
-    trainNetwork(nn);
-    printf("\n");
-    
-    // Test the network
-    testNetwork(nn);
-    
-    // Free the manually allocated memory for this network
-    free(nn);
-    free(layerDefs);
+	// set locale (for outputting formatted numbers/dates/strings)
+	setlocale(LC_ALL, "En_US");
 
-    // Calculate and print the program's total execution time
-    time_t endTime = time(NULL);
-    double executionTime = difftime(endTime, startTime);
+	// clear screen of terminal window and display title
+	clearScreen();
+	printf("MNIST-DNN: A deep neural network for MNIST image recognition \n\n");
 
-    printf("\n\nDONE! Total execution time: %.1f sec\n\n",executionTime);
-    
-    return 0;
+	// Define the network's overall architecture (layers, nodes, activation function, etc.)
+
+	// Define how many layers
+	int numberOfLayers = 4;
+
+	// Define the network model as individual layers (layer by layer)
+	LayerDefinition inputLayer = {
+		.layerType = INPUT,
+		.nodeMap = (Volume) { .width = MNIST_IMG_WIDTH, .height = MNIST_IMG_HEIGHT }
+	};
+
+	LayerDefinition hiddenLayer = {
+		.layerType = CONVOLUTIONAL,
+		.activationType = SIGMOID,
+		.nodeMap = (Volume) { .width = 24, .height = 24, .depth = 1 },
+		.filter = 5
+	};
+
+	LayerDefinition hiddenLayer2 = {
+		.layerType = CONVOLUTIONAL,
+		.activationType = SIGMOID,
+		.nodeMap = (Volume) { .width = 20, .height = 20, .depth = 1 },
+		.filter = 5
+	};
+
+	LayerDefinition outputLayer = {
+		.layerType = OUTPUT,
+		.activationType = SIGMOID,
+		.nodeMap = (Volume) { .width = 10 }
+	};
+
+	// Create an array to hold all of the above layer definitions (for easier reference throught the code)
+	LayerDefinition *layerDefs = setLayerDefinitions(numberOfLayers, inputLayer, hiddenLayer, hiddenLayer2, outputLayer);
+
+	// Display details of the network definition/architecture on the screen
+	outputNetworkDefinition(numberOfLayers, layerDefs);
+
+	// Create a neural network based on the above definition
+	Network *nn = createNetwork(numberOfLayers, layerDefs);
+
+	// Define additional hyper-parameters (optional)
+	//nn->learningRate = 0.0050;
+	nn->learningRate = 1.0;
+
+	// Train the network
+	trainNetwork(nn);
+	printf("\n");
+
+	// Test the network
+	testNetwork(nn);
+
+	// Free the manually allocated memory for this network
+	free(nn);
+	free(layerDefs);
+
+	// Calculate and print the program's total execution time
+	time_t endTime = time(NULL);
+	double executionTime = difftime(endTime, startTime);
+
+	printf("\n\nDONE! Total execution time: %.1f sec\n\n", executionTime);
+
+	return 0;
 }
 
 
